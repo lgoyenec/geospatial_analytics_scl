a1735d65096ab540ffdf00e886dcb8ac3e2d0307
Laura Goyeneche <53354917+lgoyenec@users.noreply.github.com>
2024-08-08 15:12:03 +0700

    update processing init

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -1,7 +1,7 @@
 from .population     import get_population, get_meta_url
 from .infrastructure import get_amenity_official, get_amenity
 from .connectivity   import get_tile_url
-#from .nat_disasters  import 
+from .nat_disasters  import get_desinventar, get_emdat, get_desastres
 #from .ecosystems     import 
 
 __all__ = [
@@ -9,6 +9,9 @@ __all__ = [
     'get_population',
     'get_amenity_official',
     'get_amenity',
-    'get_tile_url'
+    'get_tile_url',
+    'get_desinventar',
+    'get_emdat',
+    'get_desastres'
 ]
             
